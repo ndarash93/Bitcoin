36faccbfa7301b2b5d4739e55f3b792dcf388647
Nicholas Darash <nickdarash@gmail.com>
2025-05-21 18:07:40 -0700

    Adding ZPUB

--- electrs.py
+++ electrs.py
@@ -1,18 +1,30 @@
 import socket
 import json
 import os
-from dotenv import load_dotenv
+from bip_utils import Bip84, Bitp44Changes
 
+from dotenv import load_dotenv
 load_dotenv()
+ZPUB = os.getenv("ZPUB")
+HOST = os.getenv("HOST")
+PORT = os.getenv("PORT")
 
 class Electrs:
-  def __init__(self, params=None, host="localhost", port=50001):
+  def __init__(self, zpub, params=None, host="localhost", port=50001):
     self.params = params
     self.host = host
     self.port = port
+    self.zpub = zpub
 
-  def derive_addresses(self, zpub, gap_count=20):
-    pass
+  def derive_addresses(self, gap_count=20):
+    bip = Bip84.FromExtendedKey(self.zpub, Bip44Changes.CHAIN_EXT)
+    addresses = []
+
+    for i in range(gap_count):
+      address = bip_obh.AddressIndex(i).PublicKey().ToAddress()
+      addresses.append(address)
+
+    return addresses
 
   def electrs_request(method):
     if params is None:
@@ -31,7 +43,14 @@ class Electrs:
       return json.loads(response)
 
 # Example: get the electrs server banner
-electrs = Electrs()
-result = electrs_request("server.banner", host=os.getenv("HOST"))
-print(result)
+if __name__ == "__main__":
+  addresses = derive_address()
+  electrs = Electrs(
+    zpub=ZPUB,
+    params=None,
+    host=HOST,
+    port=PORT
+  )
+  result = electrs_request("server.banner", host=os.getenv("HOST"))
+  print(result)
 
